20dfc8a2cc748a69c76e2a04c310fb2da6178d3f
Munisa Nurboyeva <immunisanurboyeva@gmail.com>
2026-04-17 09:42:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const img = document.querySelector('.img')
 const icon = document.querySelector('.icon')
 const imgWrap = document.querySelector('.img-wrap')
 
-
 imgWrap.addEventListener('click', (e)=>{
 
     const files = Array.from(fileEl.files);
