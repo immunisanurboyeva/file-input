64a932da3f39a7d5411b928e1c7cff8790355a06
Munisa Nurboyeva <nurboyevaa@Munisas-MacBook.local>
2026-04-17 09:41:38 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const fileEl = document.querySelector('.file')
+const img = document.querySelector('.img')
+const icon = document.querySelector('.icon')
+const imgWrap = document.querySelector('.img-wrap')
+
+
+imgWrap.addEventListener('click', (e)=>{
+
+    const files = Array.from(fileEl.files);
+
+    files.forEach((file)=>{
+        img.src = URL.createObjectURL(file);
+        icon.style.display = 'none'
+    });
+});
